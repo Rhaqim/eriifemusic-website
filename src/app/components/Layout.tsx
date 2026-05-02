e505f86f09d302e5550e7a8117c52f6596815809
Rhaqim <anusiemj@gmail.com>
2026-05-02 18:49:39 +0100

    scroll to the top

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -1,13 +1,23 @@
-import { Outlet } from 'react-router';
+import { Outlet, useLocation } from 'react-router';
+import { useEffect } from 'react';
 import { Header } from './Header';
 import { Footer } from './Footer';
 import { MiniPlayer } from './MiniPlayer';
 import { ShowPromo } from './ShowPromo';
 import { MusicPlayerProvider } from '../context/MusicPlayerContext';
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+}
+
 export function Layout() {
   return (
     <MusicPlayerProvider>
+      <ScrollToTop />
       <div className="min-h-screen flex flex-col">
         <Header />
         <main className="flex-1">
